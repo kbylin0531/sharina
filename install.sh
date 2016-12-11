e162a228a35d5895b80458936fab7c7199cf98dc
lich4ung <kbylin@163.com>
2016-12-11 21:27:05 +0800

    apache permission

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
-USER=linzhv
-GROUP=linzhv
+USER=apache
+GROUP=apache
 
 mkdir Runtime
 chown -R ${USER}.${GROUP} Runtime/
 mkdir Data
 chown -R ${USER}.${GROUP} Data/
-
 mkdir Public/dynamic
 chown -R ${USER}.${GROUP} Public/dynamic/
