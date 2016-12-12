b4de660bf8040c6c9a1592cb0c1b2e73b1e8cc24
lich4ung <kbylin@163.com>
2016-12-12 09:57:13 +0800

    a

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
-USER=linzhv
-GROUP=linzhv
+#!/bin/bash
 
+# 用户和用户组设置
+USER=apache
+GROUP=apache
+
+# 运行时目录 (次要的自生成文件)
 mkdir Runtime
 chown -R ${USER}.${GROUP} Runtime/
+# 数据目录 (重要的自生成文件)
 mkdir Data
 chown -R ${USER}.${GROUP} Data/
-
+# 公共访问的自生成文件
 mkdir Public/dynamic
 chown -R ${USER}.${GROUP} Public/dynamic/
