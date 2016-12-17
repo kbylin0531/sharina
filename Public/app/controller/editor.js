b8b0237f702a84091fd11f178d14b3d77ef11e87
lich4ung <kbylin@163.com>
2016-12-18 00:06:42 +0800

    a

--- Public/app/controller/editor.js
+++ Public/app/controller/editor.js
@@ -1,17 +1,17 @@
 /**
  * Created by Lin on 2016/12/17.
  */
-angular.module('myApp', []).controller('editor', function () {
-    isea.loader.load([
-        "http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js",
-        "http://cdn.bootcss.com/summernote/0.8.2/summernote.css",
-        "http://cdn.bootcss.com/summernote/0.8.2/summernote.min.js"
-    ], function () {
-        $('.summernote').summernote({
-            height: 300,   //set editable area's height
-            codemirror: { // codemirror options
-                theme: 'monokai'
-            }
-        });
+// angular.module('myApp', []).controller('editor', function () {
+// });
+isea.loader.load([
+    "http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js",
+    "http://cdn.bootcss.com/summernote/0.8.2/summernote.css",
+    "http://cdn.bootcss.com/summernote/0.8.2/summernote.min.js"
+], function () {
+    $('.summernote').summernote({
+        height: 300,   //set editable area's height
+        codemirror: { // codemirror options
+            theme: 'monokai'
+        }
     });
 });
