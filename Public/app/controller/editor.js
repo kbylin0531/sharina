9b8fabbeb708bfba342e5fa06f1380cd9ae8364c
lich4ung <kbylin@163.com>
2016-12-18 00:08:33 +0800

    a

--- Public/app/controller/editor.js
+++ Public/app/controller/editor.js
@@ -8,6 +8,7 @@ isea.loader.load([
     "http://cdn.bootcss.com/summernote/0.8.2/summernote.css",
     "http://cdn.bootcss.com/summernote/0.8.2/summernote.min.js"
 ], function () {
+    console.log($('.summernote'));
     $('.summernote').summernote({
         height: 300,   //set editable area's height
         codemirror: { // codemirror options
