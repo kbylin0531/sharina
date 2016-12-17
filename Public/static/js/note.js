d9980dcb1640a32152d746d8e8ed38d5afecf645
lich4ung <kbylin@163.com>
2016-12-17 22:11:49 +0800

    a

---
+++ Public/static/js/note.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Lin on 2016/12/17.
+ */
+
+$('.summernote').summernote({
+    height: 300,   //set editable area's height
+    codemirror: { // codemirror options
+        theme: 'monokai'
+    }
+});
