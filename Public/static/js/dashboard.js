b1c94227a37a9984150dd665a262cd4b132f1fd0
lich4ung <kbylin@163.com>
2016-12-18 16:32:07 +0800

    a

--- Public/static/js/dashboard.js
+++ Public/static/js/dashboard.js
@@ -122,7 +122,9 @@ rdash.directive("rdLoading", function () {
     // HTML中使用"<controller name="editor" ></controller>" 调用指定将把文件"/app/controller/editor.js"引入控制器
     return {
         transclude: !0, link: function (scope, element, attrs) {
-            isea.loader.load("/app/controller/" + attrs["name"] + ".js");
+            rdash.controller("ArticleAddCtrler", function ($scope) {
+                isea.loader.load("/app/controller/" + attrs["name"] + ".js");
+            });
         }, restrict: "E"
     };
 });
