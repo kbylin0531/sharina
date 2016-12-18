b72430aae8a47b7fe5451c7e5d22e4459016f318
lich4ung <kbylin@163.com>
2016-12-18 17:17:05 +0800

    a

--- Public/app/controller/ArticleAddCtrler.js
+++ Public/app/controller/ArticleAddCtrler.js
@@ -6,28 +6,18 @@
 
 rdash.ArticleAddCtrler = {
     run: function () {
-        function run() {
+        isea.loader.load([
+            "http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js",
+            "http://cdn.bootcss.com/summernote/0.8.2/summernote.css",
+            "http://cdn.bootcss.com/summernote/0.8.2/summernote.min.js"
+        ], function () {
+
             $('.summernote').summernote({
                 height: 300,   //set editable area's height
                 codemirror: { // codemirror options
                     theme: 'monokai'
                 }
             });
-        }
-
-        var flag = false;
-
-        if (flag) {
-            run();
-        } else {
-            isea.loader.load([
-                "http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js",
-                "http://cdn.bootcss.com/summernote/0.8.2/summernote.css",
-                "http://cdn.bootcss.com/summernote/0.8.2/summernote.min.js"
-            ], function () {
-                flag = true;
-                run();
-            });
-        }
+        });
     }
 };
