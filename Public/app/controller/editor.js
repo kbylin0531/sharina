846da4d6e136ad83406b11157bd7601c20036c61
lich4ung <kbylin@163.com>
2016-12-18 16:27:21 +0800

    a

--- Public/app/controller/editor.js
+++ Public/app/controller/editor.js
@@ -3,4 +3,28 @@
  */
 // angular.module('myApp', []).controller('editor', function () {
 // });
-alert("aaa")
+rdash.controller("ArticleAddCtrler", function ($scope) {
+    function run() {
+        $('.summernote').summernote({
+            height: 300,   //set editable area's height
+            codemirror: { // codemirror options
+                theme: 'monokai'
+            }
+        });
+    }
+
+    var flag = false;
+
+    if (flag) {
+        run();
+    } else {
+        isea.loader.load([
+            "http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js",
+            "http://cdn.bootcss.com/summernote/0.8.2/summernote.css",
+            "http://cdn.bootcss.com/summernote/0.8.2/summernote.min.js"
+        ], function () {
+            flag = true;
+            run();
+        });
+    }
+});
--- Public/static/js/dashboard.js
+++ Public/static/js/dashboard.js
@@ -127,29 +127,4 @@ rdash.directive("rdLoading", function () {
     };
 });
 
-rdash.controller("ArticleAddCtrler", function ($scope) {
-    function run() {
-        $('.summernote').summernote({
-            height: 300,   //set editable area's height
-            codemirror: { // codemirror options
-                theme: 'monokai'
-            }
-        });
-    }
-
-    var flag = false;
-
-    if (flag) {
-        run();
-    } else {
-        isea.loader.load([
-            "http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js",
-            "http://cdn.bootcss.com/summernote/0.8.2/summernote.css",
-            "http://cdn.bootcss.com/summernote/0.8.2/summernote.min.js"
-        ], function () {
-            flag = true;
-            run();
-        });
-    }
-});
 
