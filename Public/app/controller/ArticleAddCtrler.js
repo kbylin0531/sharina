c81cbbb45d1cd16379423a5392fa48d6d773fcf5
lich4ung <kbylin@163.com>
2016-12-18 16:51:28 +0800

    a

--- Public/app/controller/ArticleAddCtrler.js
+++ Public/app/controller/ArticleAddCtrler.js
@@ -4,28 +4,30 @@
 // angular.module('myApp', []).controller('editor', function () {
 // });
 
-rdash.ArticleAddCtrler.run = function () {
-    function run() {
-        $('.summernote').summernote({
-            height: 300,   //set editable area's height
-            codemirror: { // codemirror options
-                theme: 'monokai'
-            }
-        });
-    }
+rdash.ArticleAddCtrler = {
+    run: function () {
+        function run() {
+            $('.summernote').summernote({
+                height: 300,   //set editable area's height
+                codemirror: { // codemirror options
+                    theme: 'monokai'
+                }
+            });
+        }
 
-    var flag = false;
+        var flag = false;
 
-    if (flag) {
-        run();
-    } else {
-        isea.loader.load([
-            "http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js",
-            "http://cdn.bootcss.com/summernote/0.8.2/summernote.css",
-            "http://cdn.bootcss.com/summernote/0.8.2/summernote.min.js"
-        ], function () {
-            flag = true;
+        if (flag) {
             run();
-        });
+        } else {
+            isea.loader.load([
+                "http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js",
+                "http://cdn.bootcss.com/summernote/0.8.2/summernote.css",
+                "http://cdn.bootcss.com/summernote/0.8.2/summernote.min.js"
+            ], function () {
+                flag = true;
+                run();
+            });
+        }
     }
 };
