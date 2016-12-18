26ab4dc1ca5b8beedbda350eeaee1b43ec67c64e
lich4ung <kbylin@163.com>
2016-12-18 17:38:58 +0800

    a

--- Public/app/controller/ArticleAddCtrler.js
+++ Public/app/controller/ArticleAddCtrler.js
@@ -6,14 +6,12 @@
 
 rdash.ArticleAddCtrler = {
     run: function () {
-        alert("aaa");
         isea.loader.load([
             "http://cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js",
             "http://cdn.bootcss.com/summernote/0.8.2/summernote.css",
             "http://cdn.bootcss.com/summernote/0.8.2/summernote.min.js"
-        ], function () {
-
-            $('.summernote').summernote({
+        ], function (islast) {
+            islast && $('.summernote').summernote({
                 height: 300,   //set editable area's height
                 codemirror: { // codemirror options
                     theme: 'monokai'
