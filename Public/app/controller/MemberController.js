3318991dfa2320fbedaee88c3eeef74282485441
lich4ung <kbylin@163.com>
2016-12-19 20:56:24 +0800

    adasa

--- Public/app/controller/MemberController.js
+++ Public/app/controller/MemberController.js
@@ -4,5 +4,10 @@
 rdash.MemberController = {
     run: function ($scope) {
 
+        $scope.saveChange = function () {
+            if ($scope.change.newpwd != $scope.change.newpwdrp) {
+                alert("aa");
+            }
+        }
     }
 };
