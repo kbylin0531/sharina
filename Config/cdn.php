e2b7d3ec74879dbd3d5cee06c514952372a7767a
linzhv <784855684@qq.com>
2016-12-19 12:40:56 +0800

    a

---
+++ Config/cdn.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by linzhonghuang.
+ * Github: git@github.com:lichtung/Sharin.git
+ * Email: 784855684@qq.com
+ * Date: 2016/12/19
+ * Time: 12:40
+ */
+return [
+    'styles' => [
+        'http://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css',
+        'http://cdn.bootcss.com/font-awesome/4.5.0/css/font-awesome.min.css',
+        'http://cdn.bootcss.com/materialize/0.97.8/css/materialize.min.css',
+        '/static/components/rdash-ui/dist/css/rdash.css',
+    ],
+    'scripts' => [
+        'http://cdn.bootcss.com/angular.js/1.5.9/angular.min.js',
+        'http://cdn.bootcss.com/angular-ui-bootstrap/1.0.3/ui-bootstrap-tpls.min.js',
+        'http://cdn.bootcss.com/angular-ui-bootstrap/1.0.3/ui-bootstrap-tpls.min.js',
+        'http://cdn.bootcss.com/angular-ui-router/0.3.2/angular-ui-router.min.js',
+        'http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js',
+        'http://cdn.bootcss.com/materialize/0.97.8/js/materialize.min.js',
+        '/isea/index.js',
+        '/static/js/dashboard.js',
+    ],
+];
