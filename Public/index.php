b9f5e8a9d8082c91775b2649440970ce0a8e5812
lich4ung <kbylin@163.com>
2016-12-21 23:08:36 +0800

    a

--- Public/index.php
+++ Public/index.php
@@ -4,7 +4,17 @@ const SR_DEBUG_MODE_ON = true;
 //包含web模块
 include __DIR__.'/../Sharin/web.inc';
 
+
 //初始化
 Sharin::register();
+
+
+if(isset($_SERVER['PATH_INFO']) and $_SERVER['PATH_INFO'] === '/admin'){
+    $url = SR_PUBLIC_URL.'/Admin/Index/index';
+//    \Sharin\Core\Response::redirect($url);
+    header("refresh:0;url={$url}");
+    exit();
+}
+
 //开启应用
 Sharin::start();
