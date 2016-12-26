daccc3eff0f5cf671e642ef578b3967e9087506d
linzhv <784855684@qq.com>
2016-12-26 13:04:07 +0800

    a

---
+++ Web/Admin/Controller/LogViewer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by linzhonghuang.
+ * Github: git@github.com:lichtung/Sharin.git
+ * Email: 784855684@qq.com
+ * Date: 2016/12/26
+ * Time: 12:41
+ */
+namespace Web\Admin\Controller;
+
+use Sharin\Core\Storage;
+
+class LogViewer
+{
+
+    public function getlist(){
+        $dir = SR_PATH_RUNTIME.'Log/';
+        $list = Storage::readdir($dir);
+        \Sharin\dumpout($list);
+    }
+
+}
